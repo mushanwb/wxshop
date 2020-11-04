dfe8a36618965ed6cadafd323a77d1883da69e3f
mushan <49012328+mushanwb@users.noreply.github.com>
2020-11-04 14:51:21 +0800

    编写完 获取登录状态的接口，并且暂时添加了返回值的内部类 (#9)
    * 编写完 获取登录状态的接口，并且暂时添加了返回值的内部类

--- src/test/java/com/mushanwb/github/wxshop/service/AuthIntegrationTest.java
+++ src/test/java/com/mushanwb/github/wxshop/service/AuthIntegrationTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static java.net.HttpURLConnection.HTTP_BAD_REQUEST;
@@ -23,7 +24,7 @@ import static java.net.HttpURLConnection.HTTP_OK;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = WxshopApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)    //随机端口
 @TestPropertySource(locations = "classpath:application.yml")
-public class CodeIntegrationTest {
+public class AuthIntegrationTest {
 
     @Autowired
     Environment environment;
@@ -38,7 +39,7 @@ public class CodeIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
                 .send(objectMapper.writeValueAsString(param))
-                .code() ;
+                .code();
         Assertions.assertEquals(HTTP_OK, responseCode);
     }
 
@@ -48,9 +49,47 @@ public class CodeIntegrationTest {
         int responseCode = HttpRequest.post(getUrl("/api/code"))
                 .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .accept(MediaType.APPLICATION_JSON_VALUE)
-                .send(objectMapper.writeValueAsString(TelVerificationServiceTest.NOT_TEL))
+                .send(objectMapper.writeValueAsString(param))
                 .code();
         Assertions.assertEquals(HTTP_BAD_REQUEST, responseCode);
+    }
+
+    @Test
+    public void loginLogoutTest() throws JsonProcessingException {
+        // 默认情况下访问 /api/status 处于未登录状态
+        // 发送验证码
+        // 带着验证码进行登录，得到 Cookie
+        // 带着 Cookie 访问 /api/status 应该处于登录接口
+        // 调用 /api/logout
+        // 再次带着 Cookie 访问 /api/status
+        String statusResponse = HttpRequest.get(getUrl("/api/status"))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .body();
+
+        Map<String, Object> response = objectMapper.readValue(statusResponse, Map.class);
+        Assertions.assertFalse((Boolean) response.get("login"));
+
+        Map<String, String> telParam = new HashMap<>();
+        telParam.put("tel", TelVerificationServiceTest.VALID_TEL);
+        int responseCode = HttpRequest.post(getUrl("/api/code"))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .send(objectMapper.writeValueAsString(telParam))
+                .code();
+        Assertions.assertEquals(HTTP_OK, responseCode);
+
+        Map<String, String> telAndCodeParam = new HashMap<>();
+        telAndCodeParam.put("tel", TelVerificationServiceTest.VALID_TEL);
+        telAndCodeParam.put("code", TelVerificationServiceTest.VALID_CODE);
+        Map<String, List<String>> responseHeaders = HttpRequest.post(getUrl("/api/login"))
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .accept(MediaType.APPLICATION_JSON_VALUE)
+                .send(objectMapper.writeValueAsString(telAndCodeParam))
+                .headers();
+        List<String> setCookie = responseHeaders.get("Set-Cookie");
+        Assertions.assertNotNull(setCookie);
+
     }
 
     private String getUrl(String apiName) {
